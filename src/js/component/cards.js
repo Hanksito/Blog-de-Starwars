747fc39c924388bd51c7d4fda5bb9455a7c5572a
Hanksito <barjabalseiro@gmail.com>
2022-07-29 18:57:25 +0000

    version final

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -1,60 +1,61 @@
-import React, { useContext,useEffect,useState } from "react";
-import {Link} from "react-router-dom"
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Card = (props) => {
   const { store, actions } = useContext(Context);
-  const {img,setImg} = useState("https://starwars-visualguide.com/assets/img/placeholder.jpg")
-  let i = "https://starwars-visualguide.com/assets/img/placeholder.jpg"
+  const [img, setImg] = useState(
+    "https://starwars-visualguide.com/assets/img/placeholder.jpg"
+  );
 
   useEffect(() => {
-			
-   getImg()
+    getImg();
   }, []);
 
   const getImg = () => {
-    fetch(`https://starwars-visualguide.com/assets/img/${props.type}/${props.id}.jpg`)
-    .then((res) =>{ 
-      if(res.ok){
-        return res.blob()
-      }}).then((imgBlob)=>{
-       
-        const imgUrl = URL.createObjectURL(imgBlob)
-        i = imgUrl
-        setImg(imgUrl.slice(5,imgUrl.length))
+    let img = `https://starwars-visualguide.com/assets/img/${props.type}/${props.id}.jpg`;
+    fetch(img).then((res) => {
+      if (res.ok) {
+        setImg(img);
+      }
+    });
+  };
 
-        
-        
-      })
-  }
-
-
-
-
-  
   return (
-      <div key={props.id} className="card m-3 wrap col-3" style={{width: "19rem"}}>
-          <img src={i} className="card-img-top" alt="..." />
-          <div className="card-body text-start">
-              <h5 className="card-title">{props.name}</h5>
-          <Link to={`/${props.type}/${props.name}/${props.id}`}>
-            <button onClick={()=>actions.getid(props.url)} type="button" className="btn btn-outline-primary btn-sm">
-              Saber mas
-            </button>
-          </Link>
-          <button type="button" onClick={props.addfavorites} className="btn btn-outline-dark btn-sm float-end">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              fill="currentColor"
-              className="bi bi-chat-square-heart-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm6 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z" />
-            </svg>
+    <div
+      key={props.id}
+      className="card m-3 wrap col-3"
+      style={{ width: "19rem" }}
+    >
+      <img src={img} className="card-img-top" alt="..." />
+      <div className="card-body text-start">
+        <h5 className="card-title">{props.name}</h5>
+        <Link to={`/${props.type}/${props.name}/${props.id}`}>
+          <button
+            onClick={() => actions.getid(props.url)}
+            type="button"
+            className="btn btn-outline-primary btn-sm"
+          >
+            Saber mas
           </button>
-        </div>
+        </Link>
+        <button
+          type="button"
+          onClick={props.addfavorites}
+          className="btn btn-outline-dark btn-sm float-end"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="25"
+            height="25"
+            fill="currentColor"
+            className="bi bi-chat-square-heart-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Zm6 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z" />
+          </svg>
+        </button>
       </div>
+    </div>
   );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,31 +7,44 @@ import { Card } from "../component/cards";
 export const Home = () => {
   const { store, actions } = useContext(Context);
   const people = store.people;
-  console.log(people);
+
   const planetas = store.planetas;
-  console.log(planetas);
+
   let img =
     "https://upload.wikimedia.org/wikipedia/commons/0/09/Luke_Skywalker_con_traje_de_piloto.jpg";
   let img2 =
     "https://www.nosolobits.com/imagenes/guia/coruscant_1843754588.jpg";
 
   return (
-    
     <div className="container-fluid ">
       <h1 className="text-start text-danger font-weight-bold my-1">
         Characters
       </h1>
       <div id={"scrolltime"} className="row flex-nowrap my-2 py-3">
         {people.map((element) => {
-          return <Card  name={element.name} id={element.uid} url={element.url} type={"characters"} addfavorites={()=> actions.updatePeopleFavs(element)}/>;
+          return (
+            <Card
+              name={element.name}
+              id={element.uid}
+              url={element.url}
+              type={"characters"}
+              addfavorites={() => actions.updatePeopleFavs(element)}
+            />
+          );
         })}
       </div>
-      <h1 className="text-start text-danger font-weight-bold my-1">
-        Planets
-      </h1>
+      <h1 className="text-start text-danger font-weight-bold my-1">Planets</h1>
       <div id={"scrolltime"} className="row flex-nowrap my-2 py-3">
         {planetas.map((element) => {
-          return <Card  name={element.name} id={element.uid} url={element.url} type={"planets"} addfavorites={()=> actions.updatePlanetFavs(element)} />;
+          return (
+            <Card
+              name={element.name}
+              id={element.uid}
+              url={element.url}
+              type={"planets"}
+              addfavorites={() => actions.updatePlanetFavs(element)}
+            />
+          );
         })}
       </div>
     </div>
